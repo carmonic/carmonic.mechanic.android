08da50f14ee57bcdd79f68b0538c5b6a597acccb
Arbeeorlar <biola.gold@gmail.com>
2019-05-17 05:12:27 +0100

    landing page and  Signup ui page

---
+++ app/src/main/java/com/camsys/carmonic/mechanic/onboarding/SignUpActivity.kt
@@ -0,0 +1,23 @@
+package com.camsys.carmonic.mechanic.onboarding
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import com.camsys.carmonic.mechanic.R
+
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
