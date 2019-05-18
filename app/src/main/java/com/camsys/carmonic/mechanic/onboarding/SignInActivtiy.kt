637b4ec29d35a40bb49e2c0151fd213d3f8315c9
Arbeeorlar <biola.gold@gmail.com>
2019-05-18 12:33:00 +0100

    Dasboard

--- app/src/main/java/com/camsys/carmonic/mechanic/onboarding/SignInActivtiy.kt
+++ app/src/main/java/com/camsys/carmonic/mechanic/onboarding/SignInActivtiy.kt
@@ -12,12 +12,8 @@ class SignInActivtiy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in_activtiy)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
     }
 
 }
--- app/src/main/java/com/camsys/carmonic/mechanic/onboarding/SignUpActivity.kt
+++ app/src/main/java/com/camsys/carmonic/mechanic/onboarding/SignUpActivity.kt
@@ -1,23 +1,72 @@
 package com.camsys.carmonic.mechanic.onboarding
 
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import com.camsys.carmonic.mechanic.R
 
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlin.math.log
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        setSupportActionBar(toolbar)
+        val infoPanel = findViewById(R.id.infoPanel) as LinearLayout
+        val passwordPanel = findViewById(R.id.passwordPanel) as LinearLayout
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
+        backbutton.setOnClickListener(View.OnClickListener {
+            showInfoPanel(infoPanel,passwordPanel)
+        })
+        btn_sign_up.setOnClickListener {
+
+            var btnSignInText = btn_sign_up.text
+            println( btnSignInText)
+
+
+
+            if(btnSignInText == "Next" ){
+
+                showInfoPanel(infoPanel,passwordPanel)
+
+            }else{
+
+                showInfoPanel(infoPanel,passwordPanel)
+
+            }
+
+        }
+
+    }
+
+    fun showInfoPanel(view1:View,view2:View) {
+         if (view1.visibility == View.VISIBLE  ){
+
+             view1.visibility = View.INVISIBLE
+             view2.visibility = View.VISIBLE
+             btn_sign_up.text = "Submit"
+
+        } else if (view2.visibility == View.VISIBLE ){
+
+             view1.visibility = View.VISIBLE
+             view2.visibility = View.INVISIBLE
+             btn_sign_up.text = "Next"
         }
     }
+
+    fun showPasswordPanel(view:View) {
+         if (view.visibility == View.VISIBLE){
+             view.visibility =  View.VISIBLE
+        } else{
+             view.visibility =  View.INVISIBLE
+        }
+    }
+
+
+
 
 }
